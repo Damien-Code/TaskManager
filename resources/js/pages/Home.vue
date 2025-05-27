c097b4b33e4c42a0880a17f3de94a41cca5ba4e4
Damien van den IJssel <dovandenijssel@live.nl>
2025-05-27 09:59:27 +0200

    more darkmode

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -30,7 +30,7 @@ defineProps<Props>();
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="relative min-h-[100vh] flex-1 rounded-xl md:min-h-min">
-                <section class="bg-white dark:bg-gray-900">
+                <section class="bg-white dark:bg-black">
                     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
                         <div class="mr-auto place-self-center lg:col-span-7">
                             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Stay Organized. Get Things Done.</h1>
