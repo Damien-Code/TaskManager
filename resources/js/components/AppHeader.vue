069b7013272974105e310cf189df094a5c0ec883
Damien van den IJssel <dovandenijssel@live.nl>
2025-05-27 10:20:14 +0200

    New logo

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -51,18 +51,18 @@ const mainNavItems: NavItem[] = [
     }
 ];
 
-const rightNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits',
-        icon: BookOpen,
-    },
-];
+// const rightNavItems: NavItem[] = [
+//     {
+//         title: 'Repository',
+//         href: 'https://github.com/laravel/vue-starter-kit',
+//         icon: Folder,
+//     },
+//     {
+//         title: 'Documentation',
+//         href: 'https://laravel.com/docs/starter-kits',
+//         icon: BookOpen,
+//     },
+// ];
 </script>
 
 <template>
