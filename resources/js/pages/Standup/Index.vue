2d9b303beae1b08d5b9fb1082703150ae9d96a35
Damien van den IJssel <dovandenijssel@live.nl>
2025-06-06 11:02:03 +0200

    Added and created new modal

--- resources/js/pages/Standup/Index.vue
+++ resources/js/pages/Standup/Index.vue
@@ -7,6 +7,15 @@ import HeadingSmall from '@/components/HeadingSmall.vue';
 import { computed } from 'vue';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { getInitials } from '@/composables/useInitials';
+import { Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger, } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -31,7 +40,48 @@ const getAvatar = (user: User) => {
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-scroll rounded-xl border p-4">
+                <div class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border p-4">
+                    <Dialog>
+                        <DialogTrigger as-child>
+                            <Button variant="outline">
+                                Create new standup
+                            </Button>
+                        </DialogTrigger>
+                        <DialogContent class="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>Edit profile</DialogTitle>
+                                <DialogDescription>
+                                    Make changes to your profile here. Click save when you're done.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <div class="grid gap-4 py-4">
+                                <div class="grid grid-cols-4 items-center gap-4">
+                                    <Label for="name" class="text-right">
+                                        Name
+                                    </Label>
+                                    <Input id="name" value="Pedro Duarte" class="col-span-3" />
+                                </div>
+                                <div class="grid grid-cols-4 items-center gap-4">
+                                    <Label for="username" class="text-right">
+                                        Username
+                                    </Label>
+                                    <Input id="username" value="@peduarte" class="col-span-3" />
+                                </div>
+                            </div>
+                            <DialogFooter>
+                                <Button type="submit">
+                                    Save changes
+                                </Button>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                </div>
+                <div
+                    class="border-sidebar-border/70 dark:border-sidebar-border relative flex aspect-video flex-col items-center justify-start overflow-hidden rounded-xl border py-4">
+                </div>
+                <div
+                    class="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-scroll rounded-xl border p-4"
+                >
                     <Heading title="Team Members" description="Invite your team members to collaborate" />
                     <div v-for="user in users" :key="user.id" class="flex flex-row gap-6 py-2">
                         <Avatar class="h-8 w-8 overflow-hidden rounded-lg">
@@ -43,15 +93,10 @@ const getAvatar = (user: User) => {
                         <HeadingSmall :title="user.name" :description="user.email" />
                     </div>
                 </div>
-                <div
-                    class="border-sidebar-border/70 dark:border-sidebar-border relative flex aspect-video flex-col items-center justify-start overflow-hidden rounded-xl border py-4">
-
-                </div>
-                <div
-                    class="border-sidebar-border/70 dark:border-sidebar-border relative flex aspect-video flex-col items-center justify-start overflow-hidden rounded-xl border py-4"
-                ></div>
             </div>
-            <div class="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min"></div>
+            <div class="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 rounded-xl border md:min-h-min">
+
+            </div>
         </div>
     </AppLayout>
 </template>
