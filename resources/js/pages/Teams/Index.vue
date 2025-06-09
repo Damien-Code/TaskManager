c568c9eee06b5045340ffaefe5e892d58e5a5f15
Damien van den IJssel <dovandenijssel@live.nl>
2025-06-09 11:32:55 +0200

    Feat: storing multiple users to team

--- resources/js/pages/Teams/Index.vue
+++ resources/js/pages/Teams/Index.vue
@@ -9,6 +9,10 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem, Team, User } from '@/types';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 import { toast } from 'vue-sonner';
+import { computed, ref } from 'vue';
+import { useFilter } from 'reka-ui';
+import { Combobox, ComboboxAnchor, ComboboxEmpty, ComboboxGroup, ComboboxInput, ComboboxItem, ComboboxList } from '@/components/ui/combobox';
+import { TagsInput, TagsInputInput, TagsInputItem, TagsInputItemDelete, TagsInputItemText } from '@/components/ui/tags-input';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -25,21 +29,31 @@ interface Props {
     teams: Team[];
     users: User[];
 }
-defineProps<Props>();
+
+const props = defineProps<Props>();
 
 const form = useForm({
     name: '',
-    user_ids: []
-})
+    user_ids: [],
+});
 const submitTeam = () => {
     form.post(route('teams.store'), {
         onSuccess: () => {
             toast.success('Team was succesfully created!');
-            form.reset()
-        }
-    })
+            form.reset();
+        },
+    });
+};
 
-}
+const open = ref(false);
+const searchTerm = ref('');
+const { contains } = useFilter({ sensitivity: 'base' });
+
+const filteredUsers = computed(() => {
+    const available = props.users.filter((user) => !form.user_ids.includes(user.id));
+    return searchTerm.value ? available.filter((user) => contains(user.name, searchTerm.value)) : available;
+});
+
 </script>
 
 <template>
@@ -49,45 +63,67 @@ const submitTeam = () => {
         <div class="mt-4">
             <Dialog>
                 <DialogTrigger as-child>
-                    <Button variant="outline"> Create new team </Button>
+                    <Button variant="outline"> Create new team</Button>
                 </DialogTrigger>
                 <DialogContent class="sm:max-w-[425px]">
                     <DialogHeader>
                         <DialogTitle>Create your new team</DialogTitle>
-                        <DialogDescription> Create a new team and start collaborating! </DialogDescription>
+                        <DialogDescription> Create a new team and start collaborating!</DialogDescription>
                     </DialogHeader>
                     <form @submit.prevent="submitTeam">
-                    <div class="grid gap-4 py-4">
-                        <div class="grid grid-cols-4 items-center gap-4">
-                            <Label for="name" class="text-right"> Team name </Label>
-                            <Input id="name" class="col-span-3" v-model="form.name"/>
-                        </div>
-                        <div class="grid grid-cols-4 items-center gap-4">
-                            <Label for="members" class="text-right"> Members </Label>
-                            <FormField v-slot="{ componentField }" name="email">
-                                <FormItem>
-                                    <Select v-bind="componentField" v-model="form.user_ids">
-                                        <FormControl>
-                                            <SelectTrigger>
-                                                <SelectValue placeholder="Select a member to add to your team" />
-                                            </SelectTrigger>
-                                        </FormControl>
-                                        <SelectContent>
-                                            <SelectGroup>
-                                                <SelectItem v-for="user in users" :key="user.id" :value="user.id">
+                        <div class="grid gap-4 py-4">
+                            <div class="grid grid-cols-4 items-center gap-4">
+                                <Label for="name" class="text-right"> Team name </Label>
+                                <Input id="name" class="col-span-3" v-model="form.name" />
+                            </div>
+                            <div class="grid grid-cols-4 items-center gap-4">
+                                <Label for="members" class="text-right"> Members </Label>
+                                <Combobox v-model="form.user_ids" v-model:open="open" :ignore-filter="true">
+                                    <ComboboxAnchor as-child>
+                                        <TagsInput v-model="form.user_ids">
+                                            <TagsInputItem v-for="id in form.user_ids" :key="id" :value="id">
+                                                <TagsInputItemText>
+                                                    {{ props.users.find((user) => user.id === id)?.name ?? 'Unknown' }}
+                                                </TagsInputItemText>
+                                                <TagsInputItemDelete @click.stop="form.user_ids = form.user_ids.filter((i) => i !== id)" />
+                                            </TagsInputItem>
+
+                                            <ComboboxInput v-model="searchTerm" as-child>
+                                                <TagsInputInput placeholder="Search users..." />
+                                            </ComboboxInput>
+                                        </TagsInput>
+
+                                        <ComboboxList>
+                                            <ComboboxEmpty>No users found</ComboboxEmpty>
+                                            <ComboboxGroup>
+                                                <ComboboxItem
+                                                    v-for="user in filteredUsers"
+                                                    :key="user.id"
+                                                    :value="user.id"
+                                                    @select.prevent="
+                                                        (ev) => {
+                                                            const val = ev.detail.value;
+                                                            if (typeof val === 'number' && !form.user_ids.includes(val)) {
+                                                                form.user_ids.push(val);
+                                                                searchTerm = '';
+                                                            }
+                                                            if (filteredUsers.length === 0) {
+                                                                open = false;
+                                                            }
+                                                        }
+                                                    "
+                                                >
                                                     {{ user.name }}
-                                                </SelectItem>
-                                            </SelectGroup>
-                                        </SelectContent>
-                                    </Select>
-                                    <FormMessage />
-                                </FormItem>
-                            </FormField>
+                                                </ComboboxItem>
+                                            </ComboboxGroup>
+                                        </ComboboxList>
+                                    </ComboboxAnchor>
+                                </Combobox>
+                            </div>
                         </div>
-                    </div>
-                    <DialogFooter>
-                        <Button type="submit"> Create team </Button>
-                    </DialogFooter>
+                        <DialogFooter>
+                            <Button type="submit"> Create team</Button>
+                        </DialogFooter>
                     </form>
                 </DialogContent>
             </Dialog>
